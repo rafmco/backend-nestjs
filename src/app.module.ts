aeaa2436f58c9cd26643101b4bac8db70e22c8ef
Rafael Machado <rafmco2@hotmail.com>
2025-10-01 12:17:45 -0400

    fix: removed unused app files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './orm-config';
@@ -8,7 +6,5 @@ import { config } from './orm-config';
 // Annotation that defines a module in NestJS
 @Module({
   imports: [TypeOrmModule.forRoot(config), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
