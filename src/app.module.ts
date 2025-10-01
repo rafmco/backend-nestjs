4057bcdae2bc96426f4a940610acf2db557dcaf0
Rafael Machado <rafmco2@hotmail.com>
2025-10-01 12:09:30 -0400

    chore: add databse config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './orm-config';
 
 // Annotation that defines a module in NestJS
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [TypeOrmModule.forRoot(config), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
